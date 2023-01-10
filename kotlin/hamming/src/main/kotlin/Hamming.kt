1eaee0b9fb916b3d2d8cd38042d8e4a9479a794d
GreatGrant <greatgrant116@gmail.com>
2023-01-10 10:12:09 +0100

    add solution to hamming exercise

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,17 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+        var hammingDistance = 0
+        var strandLength = leftStrand.length
+
+        if (leftStrand.length == rightStrand.length){
+            val rightStrandArray = rightStrand.toCharArray()
+            val leftStrandArray = leftStrand.toCharArray()
+
+            leftStrandArray.forEachIndexed{ index, _ ->
+                if (leftStrandArray[index] == rightStrandArray[index]) hammingDistance++
+            }
+        }
+        return hammingDistance
     }
 }
