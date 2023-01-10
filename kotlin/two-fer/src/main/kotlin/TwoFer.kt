743b10ce331ab678feca47396bf1533554b33b29
GreatGrant <greatgrant116@gmail.com>
2023-01-10 02:29:07 +0100

    task for Two-Fer.kt completed

--- kotlin/two-fer/src/main/kotlin/TwoFer.kt
+++ kotlin/two-fer/src/main/kotlin/TwoFer.kt
@@ -1,3 +1,1 @@
-fun twofer(name: String): String {
-    TODO("Implement the function to complete the task")
-}
+fun twofer(name: String = "you") = if (name.isEmpty()) "One for you, one for me." else "One for $name, one for me."
