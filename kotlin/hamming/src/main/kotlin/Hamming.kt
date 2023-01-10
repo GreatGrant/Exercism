6ce0ff7a9b8a23404a909ac25912d9c24bee99c8
GreatGrant <greatgrant116@gmail.com>
2023-01-10 10:45:55 +0100

    remove braces to make for concision in Hamming.kt file

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -3,13 +3,12 @@ object Hamming {
     fun compute(leftStrand: String, rightStrand: String): Int {
         var hammingDistance = 0
 
-        if (leftStrand.length == rightStrand.length){
+        if (leftStrand.length == rightStrand.length)
             leftStrand.forEachIndexed{ index, _ ->
                 if (leftStrand[index] != rightStrand[index]) hammingDistance++
             }
-        }else{
+        else
             throw IllegalArgumentException("left and right strands must be of equal length")
-        }
         return hammingDistance
     }
 }
