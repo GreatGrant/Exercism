02dec6f26d4a60f0c3168bcfd331621d2903e713
GreatGrant <greatgrant116@gmail.com>
2023-01-24 07:39:22 -0800

    solve scrable score exercise

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,10 +1,29 @@
 object ScrabbleScore {
 
+    private val letterValues = mapOf(
+        1 to listOf('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'),
+        2 to listOf('D', 'G'),
+        3 to listOf('B', 'C', 'M', 'P'),
+        4 to listOf('F', 'H', 'V', 'W', 'Y'),
+        5 to listOf('K'),
+        8 to listOf('J', 'X'),
+        10 to listOf('Q', 'Z')
+    )
+
     fun scoreLetter(c: Char): Int {
-        TODO("Implement this function to complete the task")
+        for (score in letterValues.keys){
+            if(c.toUppercase() in letterValues[score]){
+                return score
+            }
+        }
+
     }
 
     fun scoreWord(word: String): Int {
-        TODO("Implement this function to complete the task")
+        var score = 0
+        for(c in word.toUppercase())
+            score += scoreLetter(c)
+        }
+    return score
     }
 }
