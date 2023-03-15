d5d01929eda2c1ecae7bb394a5c1b10747cfb945
Great Grant <greatgrant116@gmail.com>
2023-03-15 15:04:17 +0000

    refactor code

--- kotlin/difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
@@ -1,26 +1,12 @@
 import kotlin.math.pow
 
-class Squares(val numRange: Int) {
+class Squares(val limit: Int) {
 
     // TODO: implement proper constructor
 
-    fun sumOfSquares(): Int {
-        var sum = 0
-        (1..numRange).forEach { num ->
-            sum += num.toDouble().pow(2.0).toInt()
-        }
-        return sum
-    }
+    fun sumOfSquares() =  (1..limit).map{ it * it }.sum()
 
-    fun squareOfSum(): Int {
-        var sum = 0
-        (1..numRange).forEach { num ->
-            sum += num
-        }
-        return sum.toDouble().pow(2).toInt()
-    }
+    fun squareOfSum() = (1..limit).sum().toDouble().pow(2).toInt()
 
-    fun difference(): Int {
-        return squareOfSum() - sumOfSquares()
-    }
+    fun difference() = squareOfSum() - sumOfSquares()
 }
