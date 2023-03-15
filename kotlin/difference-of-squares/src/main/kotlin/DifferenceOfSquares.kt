2573f47a4fbf02330a679c597e99fce4bf61ae6b
Great Grant <greatgrant116@gmail.com>
2023-03-15 14:56:59 +0000

    populae with solution

--- kotlin/difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
@@ -1,15 +1,26 @@
-class Squares {
-    //TODO: implement proper constructor
+import kotlin.math.pow
 
-    fun sumOfSquares() {
-        TODO("Implement the function to complete the task")
+class Squares(val numRange: Int) {
+
+    // TODO: implement proper constructor
+
+    fun sumOfSquares(): Int {
+        var sum = 0
+        (1..numRange).forEach { num ->
+            sum += num.toDouble().pow(2.0).toInt()
+        }
+        return sum
     }
 
-    fun squareOfSum() {
-        TODO("Implement the function to complete the task")
+    fun squareOfSum(): Int {
+        var sum = 0
+        (1..numRange).forEach { num ->
+            sum += num
+        }
+        return sum.toDouble().pow(2).toInt()
     }
 
-    fun difference() {
-        TODO("Implement the function to complete the task")
+    fun difference(): Int {
+        return squareOfSum() - sumOfSquares()
     }
 }
